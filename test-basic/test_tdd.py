73abcdb05f4ea12865e268904651a1ad9d48d8fc
gun <thiraphat.ps.dev@gmail.com>
2020-06-24 22:15:57 +0700

    tdd

--- test-basic/test_tdd.py
+++ test-basic/test_tdd.py
@@ -2,13 +2,17 @@
 import unittest
 
 
-#สร้าง ฟังชั่น ทีหลัง
+#สร้าง ฟังชั่น ทีหลัง         [1]
 def add_num(a, b):
     return a + b
 
 # เขียน unittest ก่อน
 class MyTest(unittest.TestCase):
-    def test_add(self):
+
+    def test_add_first(self):           #เขียนเพื่อให้ผ่าน           [2]
+        self.assertEqual(3,3)
+
+    def test_add(self):             #refactor code              [3]
         result = add_num(1, 2)
         self.assertEqual(result, 3)
 
